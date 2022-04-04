c4db10a0b3849b0f057f5a4d34af42cf1e52068e
KarimLeon <leon.karim08@gmail.com>
2022-04-04 01:23:26 -0400

    lil update on that thang

--- equations.js
+++ equations.js
@@ -1,22 +1,19 @@
-/*'use strict'
+'use strict'
 
 
-const sectionBox = document.querySelector("body > div > section.box") 
-const numArr = ['AC', '%', '/', '+', '-', 'x', 7, 8, 9, 4, 5, 6,
-               1, 2, 3, '.', 0, '=']
-const sectionDisplay = document.querySelector('body > div.calc > section.display') 
-const divCalc = document.querySelector('.calc')
+const sectionBox = document.querySelector("body > div > section.box"),
+      numArr = ['AC', '%', '/', '+', '-', 'x', 7, 8, 9, 4, 5, 6,
+               1, 2, 3, '.', 0, '='],
+      sectionDisplay = document.querySelector('body > div.calc > section.display'),
+      divCalc = document.querySelector('.calc')
 
 function createButtons(value) {
 
 for (let adder = 0; adder < value; adder++) {      
-   let numButtons = document.createElement('button') 
-     
-
-   numButtons.textContent = numArr[adder];
-  
-   sectionBox.appendChild(numButtons)
-  }
+        let numButtons = document.createElement('button') 
+        numButtons.textContent = numArr[adder];
+        sectionBox.appendChild(numButtons)
+      }
 }
 
 let span = document.querySelector('div.calc > section.display > span') 
@@ -24,8 +21,6 @@ let span = document.querySelector('div.calc > section.display > span')
 
 function buttonFunctionality() {
   const button = document.querySelectorAll('button')
-   
-
   
   //Array of number buttons
    let numbers = Object.values(button).filter(num => { 
@@ -39,41 +34,10 @@ function buttonFunctionality() {
 
    let firstVal, secVal;
 
-   
-  function Arithmetic() {
-      this.a = +firstVal;
-      this.b = +secVal;
-   }
-  const obj = new Arithmetic()
-
 
   operations.forEach(oppy => { 
     oppy.onclick = () => {
     let o = oppy.textContent
-      if (o == "AC") span.textContent = ""
-      if (o == "%") span.textContent = ""
-      if (o == "x") span.textContent = ""
-      if (o == "+") { 
-           firstVal = span.textContent
-           span.textContent = ""
-           obj["exp"] = this.a + this.b
-      } 
-      if (o == "-") span.textContent = ""
-      if (o == "/") span.textContent = ""
-
-      if (o == "AC" || "%" || 'x' || '+' || '-' || '/')  {
-           operations[6].removeAttribute('disabled'); 
-      }
-      if (o == ".") { 
-           span.textContent += o
-           operations[6].setAttribute('disabled','disabled')
-      }
-      
-      if (o == "=") {
-         secVal = span.textContent
-         span.textContent = (function()  { return  obj["exp"]})();
-      }
-            
     }
   })
 
@@ -94,78 +58,6 @@ createButtons(18)
 buttonFunctionality()
 
 */
-
-"use strict";
-
-var sectionBox = document.querySelector("body > div > section.box");
-var numArr = [
-  "AC",
-  "%",
-  "/",
-  "+",
-  "-",
-  "x",
-  7,
-  8,
-  9,
-  4,
-  5,
-  6,
-  1,
-  2,
-  3,
-  ".",
-  0,
-  "="
-];
-var sectionDisplay = document.querySelector(
-  "body > div.calc > section.display"
-);
-var divCalc = document.querySelector(".calc");
-
-function createButtons(value) {
-  for (var adder = 0; adder < value; adder++) {
-    var numButtons = document.createElement("button");
-    numButtons.textContent = numArr[adder];
-    sectionBox.appendChild(numButtons);
-  }
-}
-
-var span = document.querySelector("div.calc > section.display > span");
-
-function buttonFunctionality() {
-  var _this = this;
-
-  var button = document.querySelectorAll("button"); //Array of number buttons
-
-  var numbers = Object.values(button).filter(function (num) {
-    return !isNaN(num.textContent);
-  }); //Array of operator buttons
-
-  var operations = Object.values(button).filter(function (o) {
-    return isNaN(o.textContent);
-  });
-  var firstVal, secVal, exp;
-
-
-  operations.forEach(function (oppy) {
-    oppy.onclick = function () {
-
-      var operatorRoles = {
-         arithmetics: {
-           "+" : a + b,
-           "-" : a - b,
-           "x" : a * b,
-           "/" : a / b,
-           "%" : a % b 
-         },
-
-         other: {
-           "AC": span.textContent = "",
-           "." : null,
-           "=" : span.textContent = operatorRoles.arithmetics
-         }
-      }
 
 
 
